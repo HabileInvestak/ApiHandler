6b3f864c4414496cd372a99b577c8d90c02a80d6
Ranjithkumar <ranjithkumar.r@habile.in>
2017-01-10 13:33:44 +0530

    Api handler up to initial token url and view

--- rest_example/urls.py
+++ rest_example/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from restapp import views
 admin.autodiscover()
+
 urlpatterns = [
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^get_initial_token/', views.get_initial_token),
-	url(r'^login_2fa/', views.get_login_2fa),
-	#url(r'^user/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-	]
+   url(r'^admin/', include(admin.site.urls)),
+   url(r'^get_initial_token/', views.get_initial_token),
+   url(r'^login/', views.get_login_2fa),
+   ]
