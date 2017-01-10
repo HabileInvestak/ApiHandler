44d10229fb0935d31838a4f713d5677c2fa56695
Ranjithkumar <ranjithkumar.r@habile.in>
2017-01-10 13:35:37 +0530

    Api handler up to initial token url and view .

--- rest_example/urls.py
+++ rest_example/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from restapp import views
 admin.autodiscover()
 
+
 urlpatterns = [
    url(r'^admin/', include(admin.site.urls)),
    url(r'^get_initial_token/', views.get_initial_token),
