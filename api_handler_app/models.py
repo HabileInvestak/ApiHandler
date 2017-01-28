55ae1ad7b5f319d56abd2631e64a98b7e6c626dc
investak.habile <investak.habile@gmail.com>
2017-01-28 11:37:28 +0530

    Changes done

--- api_handler_app/models.py
+++ api_handler_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     user_id = models.TextField(primary_key=True,unique=True,null=False)
